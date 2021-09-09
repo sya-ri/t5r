50b579f3dee189eba6686116acedf85f94e5043f
sya-ri <sya79lua@gmail.com>
2021-09-09 18:43:13 +0900

    feat: TimelineController を追加 dashboard という名前を timeline に変更

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TimelineController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/', [TimelineController::class, 'show'])
+                ->middleware(['auth'])
+                ->name('timeline');
 
 require __DIR__.'/auth.php';
