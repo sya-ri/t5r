18b5de8cf285eb456e94938642d1408f06cd755c
sya-ri <sya79lua@gmail.com>
2021-09-15 12:58:39 +0900

    refactor: canLike をポリシーを使って書き換えた 指摘: https://github.com/sya-ri/t5r/pull/6#discussion_r708773356

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Gate;
 
 class Message extends Model
 {
@@ -30,14 +31,9 @@ class Message extends Model
         return $this->hasMany(Like::class)->firstWhere('user_id', $user->id);
     }
 
-    public function canLike($user)
-    {
-        return $user->id !== $this->user_id;
-    }
-
     public function like($user)
     {
-        if ($this->canLike($user)) {
+        if (Gate::forUser($user)->allows('like', $this)) {
             Like::create(['user_id' => $user->id, 'message_id' => $this->id]);
         }
     }
